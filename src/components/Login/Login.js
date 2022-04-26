d75292ed643dc3d24fb019537013500c1f2bd3de
devshaik444 <mamin172025@bsces.uiu.ac.bd>
2022-04-26 16:34:44 +0600

    new commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase'
+
+import './login.css'
 
 const Login = () => {
+    const [user,signInwithGoogle]=useFirebase()
+    // console.log(user,signInwithGoogle)
     return (
-        <div>
+        <div className='login-div'>
             <h1>Please Login</h1>
+
+            <div style={{marginBottom:'10px'}}>
+                <button onClick={signInwithGoogle} style={{borderRadius:'15px',backgroundColor:'blue',color:'white',border:'1px solid blue'}}>Google Sign In</button>
+            </div>
+            
+            <form>
+                <input type='text' placeholder='Email'/>
+                <br/>
+                <input type='password' placeholder='Password' />
+                <br/>
+                <input type='submit' value='Login'  />
+            </form>
         </div>
     );
 };
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,8 +3,17 @@ import './register.css'
 
 const Register = () => {
     return (
-        <div>
-            
+        <div className='register-div'>
+            <h1>Please Register now</h1>
+            <form>
+                <input type='text' placeholder='Your Name'/>
+                <br/>
+                <input type='text' placeholder='Email'/>
+                <br/>
+                <input type='password' placeholder='Password' />
+                <br/>
+                <input type='submit' value='Register'  />
+            </form>
         </div>
     );
 };
